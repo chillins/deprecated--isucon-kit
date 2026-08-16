43ebfa8fdf3a8974de99de34263d029cdd4b6410
1keiuu <ikkei12.h@gmail.com>
2026-08-16 11:49:12 +0900

    update: exclude unneccesary language direcotry when pulling

--- pull.sh
+++ pull.sh
@@ -2,7 +2,20 @@
 
 set -ue -o pipefail
 
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+set -a
+source "${SCRIPT_DIR}/config.env"
+set +a
+
+LANGUAGES=(go node perl php python ruby rust)
+
 # Pull application code & nginx, mysql config file
-rsync --filter=":- .gitignore" -av isuconapp:/home/isucon/webapp/ ./webapp/
+rsync_args=(--filter=":- .gitignore" -av)
+for lang in "${LANGUAGES[@]}"; do
+  if [[ "${lang}" != "${LANGUAGE}" ]]; then
+    rsync_args+=(--exclude "${lang}/")
+  fi
+done
+rsync "${rsync_args[@]}" isuconapp:/home/isucon/webapp/ ./webapp/
 rsync -av isuconapp:/etc/nginx/ ./nginx/
 rsync -av isucondb:/etc/mysql/ ./mysql/
