ab69d1fdffdc0dbdd105a3350030be7a47769bbb
Ikkei Harashima <46051957+1keiuu@users.noreply.github.com>
2026-08-21 16:54:06 +0900

    Revert "feat: pprotein 💪 😅 💪"

--- install_tools.sh
+++ install_tools.sh
@@ -9,11 +9,3 @@ ssh isuconapp 'make nginx/install-alp'
 # Install analyzer for DB
 rsync -av ./Makefile isucondb:~ 
 ssh isucondb 'make mysql/install-pt-query-digest'
-
-# Install pprotein for app
-rsync -av ./systemd/pprotein.service ./systemd/pprotein-agent-httplog.service isuconapp:/etc/systemd/system/
-ssh isuconapp 'make pprotein/setup-app'
-
-# Install pprotein for db
-rsync -av ./systemd/pprotein-agent.service isucondb:/etc/systemd/system/
-rsync -av ./mysql/allow-remote.sql isucondb:/etc/mysql/
